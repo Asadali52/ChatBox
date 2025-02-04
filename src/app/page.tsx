1123db80e9490bd03c8ced7def1ca5b67d9a581b
Asad Ali <Asad Ali>
2025-02-04 18:06:45 +0500

    hove btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-screen justify-center items-center">
       <Link href='/chatBox'>
-      <p className="text-[15px] h-[60px] leading-[60px] px-10 rounded-xl cursor-pointer font-bold bg-[#0A96D4] text-white">
+      <p className="text-[15px] h-[60px] leading-[60px] px-10 rounded hover:bg-black cursor-pointer font-bold bg-[#0A96D4] text-white">
         Open Chatbox
       </p>
       </Link>     
